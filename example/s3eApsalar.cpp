d75d125b303557eb8ba5121711a5293b0f094bd8
deemen <danmenard@gmail.com>
2013-03-06 09:02:19 -0500

    Improved example

--- example/s3eApsalar.cpp
+++ example/s3eApsalar.cpp
@@ -1,19 +1,15 @@
-#include "ExamplesMain.h"
 #include "s3eOSReadString.h"
 #include "s3eApsalar.h"
 #include "IwDebug.h"
+#include "IwGx.h"
 #include "s3eExt.h"
+#include "s3ePointer.h"
 
 const char *yourApiKey = "s3eApsalar";
 const char *yourApiSecret = "6TWtvU2K";
 
-void LogEvent();
-
-//Example Main
-void ExampleInit()
+void initApsalar()
 {
-    s3eDebugOutputString("Booting s3eApsalar example");
-
     if (!s3eApsalarAvailable())
     {
         s3eDebugOutputString(s3eExtGetErrorString());
@@ -22,26 +18,59 @@ void ExampleInit()
 
     s3eDebugOutputString("Successfully loaded s3eApsalar extension");
 
-    s3eDebugOutputString("Starting Apsalar session");
     s3eApStart(yourApiKey, yourApiSecret);
-
     s3eDebugOutputString("Apsalar session started");
+}
 
+void logEvent()
+{
     s3eApLogEvent("TestEvent");
     s3eDebugOutputString("TestEvent logged");
 }
 
-bool ExampleUpdate()
+void shutdownApsalar()
 {
-    s3eDebugOutputString("Updating");
-    return true;
+    s3eApEnd();
+    s3eDebugOutputString("Apsalar Shutdown");
 }
 
-void ExampleRender()
+void touchEvent(s3ePointerTouchEvent *event)
 {
-    s3eDebugOutputString("Rendering");
+    if(event->m_Pressed)
+    {
+        logEvent();
+    }
 }
 
-void ExampleShutDown()
+// Example showing how to use the s3eApsalar extension
+int main()
 {
+    s3eDebugOutputString("Booting s3eApsalar example");
+
+    s3ePointerRegister(S3E_POINTER_BUTTON_EVENT, (s3eCallback)touchEvent, NULL);
+
+    IwGxInit();
+
+    IwGxSetColClear(0, 0, 0, 0xff);
+
+    initApsalar();
+
+    while(!s3eDeviceCheckQuitRequest())
+    {
+        IwGxClear();
+
+        IwGxPrintString(100, 100, "s3eApsalar");
+
+        IwGxPrintString(100, 300, "Touch to fire event");
+
+        IwGxFlush();
+        IwGxSwapBuffers();
+        s3eDeviceYield(0);
+    }
+
+    shutdownApsalar();
+
+    IwGxTerminate();
+
+    return 0;
 }
